aef0d31f0f5a16333b9e7f61e2629db982207ddf
imss003 <itsmeshubham003@gmail.com>
2024-06-01 20:21:36 +0530

    added comments

--- Javascript_project_BMI_calculator/script.js
+++ Javascript_project_BMI_calculator/script.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('form');
 console.log(form);
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
+form.addEventListener('submit', (e) =>{ //notice that the event here is submit
+    e.preventDefault(); //to prevent the default behaviour of form of submitting info to server, since we dont want the info to go to server rather we want to use it
     let height = document.querySelector('#height').value //to get the value of height input by user
     let weight = document.querySelector('#weight').value //to get the value of weight input by user
     console.log(height, weight);
