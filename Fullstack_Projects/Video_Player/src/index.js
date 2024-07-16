60950cc883387c50713180dc9d95ca4dd630915a
imss003 <itsmeshubham003@gmail.com>
2024-07-16 20:54:47 +0530

    add user and video model

--- Fullstack_Projects/Video_Player/src/index.js
+++ Fullstack_Projects/Video_Player/src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import express from "express"
 
+const app = express();
 dotenv.config({
     path:"./env"
 })
-connectDB();
+connectDB()
+.then(() => {
+    app.on('error', (err) => {
+        console.log("Server connection FAILED", err)
+    })
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is listening at port: ${process.env.PORT}`);
+    })
+})
+.catch(err => console.log(`MongoDB connection FAILED`, err))
 
